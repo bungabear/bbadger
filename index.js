8e16b6ade3392338fec7a49e3fc241e39aaad922
Minjae Son <bungabear6422@gmail.com>
2025-02-04 11:13:48 +0900

    File test for font issue

--- index.js
+++ index.js
@@ -5,6 +5,21 @@ const openBadge = require('openbadge');
 const axios = require('axios');
 const xml2js = require('xml2js');
 
+const handler = require('serve-handler');
+const http = require('http');
+
+const server = http.createServer((request, response) => {
+  // You pass two more arguments for config and middleware
+  // More details here: https://github.com/vercel/serve-handler#options
+  return handler(request, response);
+});
+
+server.listen(port, () => {
+  console.log(`Running at http://localhost:${port}`);
+});
+
+return;
+
 function makeBadge(first, second, firstColor, secondColor){
     return new Promise(function(resolve, reject){
         const color = {};
